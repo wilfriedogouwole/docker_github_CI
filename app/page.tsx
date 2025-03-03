e95fa33f847ef85bfeed93cc53909ba175a60d93
dev2 <dev2@dev2>
2025-03-03 11:13:15 +0100

    v3

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="bg-amber-200 flex flex-col items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-  <p>Test docker CI/CD action github on vps version ????jj</p>
+  <p>Test docker CI/CD action github on vps version ????</p>
     </div>
   );
 }
